1c11bd727153f7fd21b2979d175e0841b970b663
Derek <derek.schaefer@gmail.com>
2017-09-18 21:25:19 -0500

    remove default module stuffs

--- lib/plug_metrics.ex
+++ lib/plug_metrics.ex
@@ -1,18 +1,2 @@
 defmodule PlugMetrics do
-  @moduledoc """
-  Documentation for PlugMetrics.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PlugMetrics.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/plug_metrics_test.exs
+++ test/plug_metrics_test.exs
@@ -1,8 +1,2 @@
 defmodule PlugMetricsTest do
-  use ExUnit.Case
-  doctest PlugMetrics
-
-  test "greets the world" do
-    assert PlugMetrics.hello() == :world
-  end
 end
