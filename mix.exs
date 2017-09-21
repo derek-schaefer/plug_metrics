d6ecbcbb83802cf4d24c36a8582c0510d6e99369
Derek <derek.schaefer@gmail.com>
2017-09-20 22:14:33 -0500

    version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugMetrics.Mixfile do
   def project do
     [
       app: :plug_metrics,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
